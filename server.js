d98e814d27329e067cba1c5b82b906e6f9ac8bd2
Moises <moiseslsth2204@gmail.com>
2018-12-31 11:49:30 -0400

    Cambiando el puerto local al por defecto

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ const server = http.createServer(app);
 
 /*CONEXION A BASE DE DATO MONGODB*/
 
-mongoose.connect("mongodb://localhost:27017/bonodb", { useNewUrlParser: true }, (error, response) => {
+mongoose.connect("mongodb://mongo:27017/bonodb", { useNewUrlParser: true }, (error, response) => {
     if(error){
         throw error;
     }else{
         console.log("Se realizo la conexion a bonodb correctamente");
 
         app.listen(port, function(){
-            console.log("Servidor establecido en http://localhost:"+port)
+            console.log("Servidor establecido en http://mongo:"+port)
         })
     }
 
